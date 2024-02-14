2eea995c1959594bd42d1d203c7235bbbec6d06a
NAKATA Maho <maho.nakata@gmail.com>
2024-02-14 17:51:39 +0900

    .

--- setup/setup_flint.sh
+++ setup/setup_flint.sh
@@ -1,24 +1,25 @@
 MPFR_VERSION=4.2.1
 GMP_VERSION=6.3.0
-VERSION=1.3.1
+VERSION=3.0.1
 export CC=gcc-12
 export CXX=g++-12
-export CFLAGS="-Wall -O3 -march=zen2"
+export CFLAGS="-Wall -O3 -march=znver2"
 
-WRKDIR=/home/docker/flintcxx/setup/work_mpc
+WRKDIR=/home/docker/flintcxx/setup/work_flint
 ARCHIVEDIR=/home/docker/flintcxx/archives/
 
 rm -rf $WRKDIR
 mkdir -p $WRKDIR
 cd $WRKDIR
-tar xvf $ARCHIVEDIR/mpc-${VERSION}.tar.gz
-cd mpc-${VERSION}
+tar xvf $ARCHIVEDIR/flint-${VERSION}.tar.gz
+cd flint-${VERSION}
 ./configure                                         \
     --enable-assert                                 \
     --enable-avx2                                   \
     --disable-static                                \
     --with-gmp-include=/home/docker/flintcxx/i/GMP-${GMP_VERSION}/include \
-    --with-mpfr=/home/docker/flintcxx/i/MPFR-${MPFR_VERSION} \
+    --with-gmp-lib=/home/docker/flintcxx/i/GMP-${GMP_VERSION}/lib         \
+    --with-mpfr=/home/docker/flintcxx/i/MPFR-${MPFR_VERSION}              \
     --prefix=/home/docker/flintcxx/i/FLINT-${VERSION}
 make -j`nproc`
 make check
